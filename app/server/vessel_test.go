da2549c3b923b554eef863ad121374294fa40ec9
Victor Barros <victorbarros1130@gmail.com>
2020-09-01 20:37:52 -0300

    Improve insert vessel tests

--- app/server/vessel_test.go
+++ app/server/vessel_test.go
@@ -9,9 +9,65 @@ import (
 )
 
 func TestInsertVessel(t *testing.T) {
-	err := postVessel("MV101")
-	if err != nil {
-		t.Error(err)
+	tests := []struct {
+		vessel  string
+		success bool
+	}{
+		{
+			vessel:  "MV101",
+			success: true,
+		},
+		{
+			vessel:  "MV101",
+			success: false,
+		},
+	}
+
+	for _, test := range tests {
+		err := postVessel(test.vessel)
+		if (err != nil) == test.success {
+			t.Error(err)
+		}
+	}
+}
+
+func TestInsertVesselBadRequest(t *testing.T) {
+	tests := []struct {
+		body interface{}
+	}{
+		{
+			body: struct {
+				Code int `json:"code"`
+			}{
+				4,
+			},
+		},
+		{
+			body: struct {
+				Code string `json:"code"`
+			}{
+				"",
+			},
+		},
+	}
+
+	for _, test := range tests {
+		decode, err := json.Marshal(test.body)
+		if err != nil {
+			t.Error(err)
+		}
+
+		endpoint := fmt.Sprint(baseRoute, "vessel")
+		resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(decode))
+		if err != nil {
+			t.Error(err)
+		}
+
+		if resp.StatusCode != http.StatusBadRequest {
+			respBody := response{}
+			json.NewDecoder(resp.Body).Decode(&respBody)
+			t.Error("Fail to make bad request")
+		}
 	}
 }
 
